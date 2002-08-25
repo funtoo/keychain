51af330b52856c6671e126c23f51d16fe3af71b5
Daniel Robbins <drobbins@funtoo.org>
2002-08-25 01:00:10 -0400

    [svn] 2.0.1

--- tarball.sh
+++ tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-V=2.0
+V=2.0.1
 rm -rf /tmp/keychain-${V}
-mkdir /tmp/keychain-${V}; cp keychain keychain.* README ChangeLog /tmp/keychain-${V}
+mkdir /tmp/keychain-${V}; cp keychain README ChangeLog /tmp/keychain-${V}
 chown -R root.root /tmp/keychain-${V}
 cd /tmp; tar cjvf keychain-${V}.tar.bz2 keychain-${V}
