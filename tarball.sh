136ed1df9e8170d5a73723e697beeff7e59d7b01
Seth Chandler <sethbc@gentoo.org>
2003-04-17 14:40:59 -0400

    [svn] new versions, not ready for primetime yet, so its not quite released...

--- tarball.sh
+++ tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-V=2.0.2
+V=2.0.3
 rm -rf /tmp/keychain-${V}
-mkdir /tmp/keychain-${V}; cp keychain README ChangeLog /tmp/keychain-${V}
+mkdir /tmp/keychain-${V}; cp keychain README ChangeLog keychain.1 /tmp/keychain-${V}
 chown -R root.root /tmp/keychain-${V}
 cd /tmp; tar cjvf keychain-${V}.tar.bz2 keychain-${V}
