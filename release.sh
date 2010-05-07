e9ec1b81e95b67f486dbba4b3d3b6a1af2bf2707
Daniel Robbins <drobbins@funtoo.org>
2010-05-07 11:24:14 -0600

    2.7.1 distribution release

--- release.sh
+++ release.sh
@@ -30,9 +30,9 @@ commit() {
 	# for the maintainer, we don't want to pass this complexity on to the consumers of this
 	# package.
 
-	#git commit -a -m "$VERSION distribution release" || die "commit failed"
+	git commit -a -m "$VERSION distribution release" || die "commit failed"
 	git archive --format=tar --prefix=${PKG}-${VERSION}/ HEAD | tar xf - -C dist || die "git archive fail"
-	#git push || die "keychain git push failed"
+	git push || die "keychain git push failed"
 	cd dist/$PKG-$VERSION || die "pkg cd fail"
 	# tmpclean target cleans up temporary intermediate files that were produced.
 	make clean all || die "make dist failed"
@@ -45,9 +45,9 @@ commit() {
 web() {
 	cp dist/$PKG-$VERSION.tar.bz2 /root/git/website/archive/$PKG/ || die "web cp failed"
 	cd /root/git/website || die "cd failed"
-	#git add archive/$PKG/* || die "git add failed"
-	#git commit -a -m "new $PKG $VERSION" || die "git commit failed"
-	#git push || die "git push failed"
+	git add archive/$PKG/* || die "git add failed"
+	git commit -a -m "new $PKG $VERSION" || die "git commit failed"
+	git push || die "git push failed"
 	./install.sh || die "web update failed"
 }
 
